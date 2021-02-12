5c191b7a8255def8c9c6a4abe65b3286f3bc53a6
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-12 22:39:29 +0300

    delete components Question, Answer. refactor CountryCapital.ts, easy game works

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import 'antd/dist/antd.css'
-import {Answer} from './components/Answer/Answer';
-import {Question} from './components/Question/Question';
-import {randomCountry} from '../src/components/CountryCapital'
+import {randomCountry} from './components/CountryCapital'
+import {Button, Input} from 'antd';
 
 function App() {
 
-    let country = randomCountry()
+    const [isChecked, setIsChecked] = useState<boolean>(false);
+    const [country, setCountry] = useState<Array<string>>(randomCountry);
+    const [userAnswer, setUserAnswer] = useState<string>('');
+    const [right, setRight] = useState<boolean>(false);
+
+    const onCheckClick = () => {
+        if(userAnswer === country[1]) {
+            setRight(true)
+        } else {
+            setRight(false)
+        }
+        setIsChecked(true)
+    }
+
+    const onNextClick = () => {
+        setIsChecked(false)
+        setCountry(randomCountry())
+    }
+
+    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(e.currentTarget.value)
+    }
 
     return (
-        <div className="App">
-            <Question country={country[0]}/>
-            <Answer capital={country[1]}/>
+        <div>
+            <div>
+                Столица {country[0]}?
+            </div>
+            <div>
+                <Input value={userAnswer} onChange={onInputChange}></Input>
+            </div>
+            <div>
+                <Button onClick={onCheckClick}>Check</Button>
+            </div>
+            {isChecked ?
+                <div>
+                    {right ? 'Правильно!' : 'Неверно!'} Ответ: {country[1]}
+                    <Button onClick={onNextClick}>Next</Button>
+                </div>
+                : ''
+            }
         </div>
     );
 }
