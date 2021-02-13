0c7d2539b4f1445ab41dd4dbcf7d3aa23b702846
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-13 13:42:04 +0300

    add leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import './App.css';
 import 'antd/dist/antd.css'
 import {randomCountry} from './components/CountryCapital'
 import {Button, Input} from 'antd';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 function App() {
 
     const [isChecked, setIsChecked] = useState<boolean>(false);
-    const [country, setCountry] = useState<Array<string>>(randomCountry);
+    const [country, setCountry] = useState<{name: string, capital: string}>(randomCountry);
     const [userAnswer, setUserAnswer] = useState<string>('');
     const [right, setRight] = useState<boolean>(false);
 
     const onCheckClick = () => {
-        if(userAnswer === country[1]) {
+        if (userAnswer === country.capital) {
             setRight(true)
         } else {
             setRight(false)
@@ -29,10 +30,11 @@ function App() {
         setUserAnswer(e.currentTarget.value)
     }
 
+
     return (
         <div>
             <div>
-                Столица {country[0]}?
+                Столица {country.name}?
             </div>
             <div>
                 <Input value={userAnswer} onChange={onInputChange}></Input>
@@ -42,11 +44,25 @@ function App() {
             </div>
             {isChecked ?
                 <div>
-                    {right ? 'Правильно!' : 'Неверно!'} Ответ: {country[1]}
+                    {right ? 'Правильно!' : 'Неверно!'} Ответ: {country.capital}
                     <Button onClick={onNextClick}>Next</Button>
                 </div>
                 : ''
             }
+            <div className={'mapContainer'}>
+                <MapContainer className={'map'} center={[51.505, -0.09]} zoom={5} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC'
+                        url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br/> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+
         </div>
     );
 }
